3fa023703e6a2a267d9117ba98ba92cbcbf351a7
worker2of2month <workerofmonth@usa.com>
2023-07-03 04:31:25 +0300

    Fuck this shit

--- Lua/Waves/Stalker_trident.lua
+++ Lua/Waves/Stalker_trident.lua
@@ -5,7 +5,7 @@ last_beep_time = 0 -- время, через которое спрайт вос�
 warning_beep_time = 3 -- константа
 
 attack_number = 0 -- это - 1 = сколько копий уже было брошено
-fireball_timer = 0
+spawn_shit_timer = 0
 
 --epsilon = 5e-4
 
@@ -14,6 +14,9 @@ Encounter["wavetimer"] = math.huge
 fireballs = {}
 emitters = {}
 
+FireWallEmitters = {}
+FireWall = {}
+
 Arena.Resize(150,150)
 
 CreateLayer("Ugly", "Top", true)
@@ -21,25 +24,55 @@ Void = CreateSprite("masks/Arena_150x170", "Ugly", -1)
 Void.MoveTo(Arena.x - 155, Arena.y + 89)
 Void2 = CreateSprite("masks/Arena_150x170", "Ugly", -1)
 Void2.MoveTo(Arena.x - 155 - 150, Arena.y + 89)
+Void3 = CreateSprite("masks/Void_sprite", "Ugly", -1)
+Void3.MoveTo(Arena.x - 10, Arena.y * 2 + 77)
+
+arenaover = CreateSprite('masks/arenaover', "Ugly", -1)
+arenaover.Scale(0.765, 0.765)
+arenaover.MoveTo(Arena.x + 0.4, Arena.y * 2 - 10)
+
+--Border = CreateSprite("masks/fwall", "Ugly", -1)
+--Border.MoveTo(Arena.x - 155 - 150, Arena.y + 89)
 
 function Update()
 
-    if firewall_status == true then
+
+
+    if spawn_shit_timer % 7 == 0 and #emitters == 6 and #fireballs <= 100 then
+        for i=1,6 do
+            create_fireball(emitters[i])
+        end
+    end
+
+    if wavy_fire_status== true and #emitters == 6 then
         for i = 1, #fireballs do
             update_fireball(fireballs[i])
         end
     end
 
-    --DEBUG(Player.y)
-
-    fireball_timer = fireball_timer + 1
-    --spawntimer = spawntimer + 1
-
-    if fireball_timer % 10 == 0 and #emitters == 6 and #fireballs <= 100 then
-        for i=1,6 do
-            create_fireball(emitters[i])
+    if #FireWallEmitters == 2 and #FireWall <= 66 and spawn_shit_timer % 15 == 0 then
+        for i=1,2 do
+            fire_wall_el_spawn(FireWallEmitters[i])
         end
     end
+
+    if #FireWallEmitters == 2 then
+        for i=#FireWall, 1 , -1 do
+            local current_bullet = FireWall[i]
+
+            if current_bullet.y < -Arena.height/2 + 10then
+                table.remove(FireWall, i)
+                current_bullet.Remove()
+            else
+                fire_wall_update(FireWall[i])
+            end
+        end
+    end
+
+    --DEBUG(Player.y)
+
+    spawn_shit_timer = spawn_shit_timer + 1
+    --spawntimer = spawntimer + 1
 
     --[[CreateLayer("void", "BelowUI", true)
     void = CreateSprite("void")
@@ -48,7 +81,7 @@ function Update()
     beep_update()
     if spawntimer == 4 then
         Audio.PlaySound("bwoar")
-        --create_trident()
+        create_trident()
     end
 
     if spawntimer > 45 and spawntimer < 100 and bullet.x != -15 then
@@ -74,22 +107,62 @@ function Update()
 
 end
 
-function firewall_configuration(x) -- firewall_number - номер волны огня 
+function firewall_configuration()
+
+    local wall_r = {}
+    local wall_l = {}
+
+    wall_l.amplitude = 0.95
+	wall_r.amplitude = 0.95
+
+    wall_r.init_x = -150
+
+    wall_l.init_x = wall_r.init_x + 255
+
+    table.insert(FireWallEmitters, wall_l)
+    table.insert(FireWallEmitters, wall_r)
+
+end
+
+function fire_wall_el_spawn(wallemitter)
+
+    local fire_wall_el = CreateProjectile("fire_wall", wallemitter.init_x, 69)
+
+    fire_wall_el.SetVar("amplitude", wallemitter.amplitude)
+    fire_wall_el.SetVar("time_from_beggin", 0)
+
+    table.insert(FireWall, fire_wall_el)
+
+end
+
+function fire_wall_update(fire_wall_el)
+
+    local time_from_beggin = fire_wall_el.GetVar("time_from_beggin") + 1
+    fire_wall_el.SetVar("time_from_beggin", time_from_beggin)
+
+    local Newx = fire_wall_el.GetVar("amplitude") * math.cos(time_from_beggin / 40 )
+
+    fire_wall_el.Move(Newx, -0.8)
+end
+
+firewall_configuration()
+
+function wavy_fire_configuration(x) -- firewall_number - номер волны огня 
 
     local emitter_r = {}
     local emitter_l = {}
 
-    emitter_l.amplitude = -0.5
-	emitter_r.amplitude = 0.5
+    emitter_l.amplitude = -0.95
+	emitter_r.amplitude = 0.95
 
-    emitter_r.init_y = 220
-    emitter_r.init_x = -80 + x * 40
+    emitter_r.init_y = 222 + x * 15
+    emitter_r.init_x = -110 + x * 55
     emitter_r.y_velocity = -2.85
 
     emitter_r.index = #emitters + 1
 
-    emitter_l.init_y = 220
-    emitter_l.init_x = -80 + x * 40
+    emitter_l.init_y = 222 + x * 15
+    emitter_l.init_x = -110 + x * 55
     emitter_l.y_velocity = -2.85
 
     emitter_l.index = #emitters
@@ -97,11 +170,17 @@ function firewall_configuration(x) -- firewall_number - номер волны о
     table.insert(emitters, emitter_l)
     table.insert(emitters, emitter_r)
 
-    firewall_status = true 
+    wavy_fire_status= true 
 end
 
+--for i=1,3 do
+--	wavy_fire_configuration(i)
+--end
+
 function create_fireball(emitter)
+
     local fireball = CreateProjectile("hitbox_fire", emitter.init_x, emitter.init_y)
+    fireball.sprite.Scale (0.95, 0.95)
     fireball.sprite.Set("firebullet0")
 
     fireball.SetVar("time_from_beggin", 0)
@@ -114,11 +193,12 @@ function create_fireball(emitter)
 
 
     table.insert(fireballs, fireball)
+
 end
 
 function update_fireball(fireball)
 
-    local time_from_beggin = fireball.GetVar("time_from_beggin", 0) + 1
+    local time_from_beggin = fireball.GetVar("time_from_beggin") + 1
     fireball.SetVar("time_from_beggin", time_from_beggin)
 
     --[[if math.abs(1 - math.cos(time_from_beggin / 40)) <= epsilon and Time.time - fireball.GetVar("amp_mul_cooldown") >= 40  then
@@ -131,11 +211,12 @@ function update_fireball(fireball)
     end]]
 
     if fireball.GetVar("init_x") == fireball.x and fireball.GetVar("Period") == 0 then 
-        fireball.SetVar("amp_mul", 1/10)
+        fireball.SetVar("amp_mul", 1/55)
         fireball.SetVar("Period", 1)
         fireball.SetVar("Period_timer", 0)
-    elseif fireball.GetVar("Period") == 1 and fireball.GetVar("Period_timer") == 15 then
+    elseif fireball.GetVar("Period") == 1 and fireball.GetVar("Period_timer") == 5 then
         fireball.SetVar("amp_mul", 1)
+        fireball.SetVar("Period", 0)
     end
 
     if fireball.GetVar("Period") == 1 then
@@ -151,22 +232,17 @@ function update_fireball(fireball)
 
     local amplitude = fireball.GetVar("amplitude") * amp_mul
 
-    local Newx = amplitude * math.cos(time_from_beggin / 40 * 2)
+    local Newx = 1.5 * amplitude * math.cos(time_from_beggin / 40 * 4)
+
+    fireball.Move(Newx, -1.9)
 
     --DEBUG(tostring(Newx))
-
-    fireball.Move(Newx, -1.5)
  
     --[[for key, value in pairs(fireballs) do
         DEBUG(key, value)
     end]]
 
 end
-
-for i=1,3 do
-	firewall_configuration(i)
-end
-
 
 function create_trident()
     if Player.y < -43 then
